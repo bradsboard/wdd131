5fa287f21c524b3f7086d06ba1f09b6380a24713
bradsboard <bradsboard@gmail.com>
2025-05-15 15:32:13 -0600

    fav chapter push

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -1,13 +1,29 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
+const list = document.querySelector('#list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
 //populating content
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
 
+
+button.addEventListener('click', function () {
+    //code to be exectued
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+
+        });
+
+        input.value = '';
+        input.focus();
+    }
+});   
