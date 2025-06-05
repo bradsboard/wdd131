1fb34d403de4439de5864ae3a17813d5c490a779
bradsboard <bradsboard@gmail.com>
2025-06-05 12:02:06 -0600

    test

---
+++ project/siteplan.js
@@ -0,0 +1,9 @@
+//date modifyer
+const yearSpan = document.getElementById("currentyear");
+const lastModSpan = document.getElementById("lastModified");
+
+// Insert the current year using date
+yearSpan.textContent = new Date().getFullYear();
+
+// Insert last modified date in native format veryyyy important
+lastModSpan.textContent = document.lastModified;
